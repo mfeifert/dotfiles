14b412354906bb1901db3bf3f027faf520d02f22
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-03-23 11:16:02 -0700

    nixos: Move sytem and user configurations into separate files

---
+++ nixos/user.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+    ];
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  users.users.maf = {
+    isNormalUser = true;
+    description = "Matthew";
+    extraGroups = [ "networkmanager" "wheel" "wireshark" "libvirtd" ];
+    packages = with pkgs; [
+      firefox
+      dropbox
+      emacs29-gtk3
+      ed
+      nvi
+      vim
+      neovim
+      helix
+      micro
+      ledger
+      hledger
+      mpv
+      wireshark
+      yazi
+      mc
+      ripgrep
+      fzf
+      bat
+      sqlite
+      sqlitebrowser
+      neofetch
+      eza
+      nerdfonts
+    ];
+  };
+
+#  These git settings only work in Home Manager
+#  programs.git = {
+#    enable = true;
+#    userName = "Matthew Feifert";
+#    userEmail = "55014056+mfeifert@users.noreply.github.com";
+#    extraConfig = {
+#      init.defaultBranch = "main";
+#    };
+#  };
+
+  programs.wireshark.enable = true;
+  programs.virt-manager.enable = true;
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
+
+}
