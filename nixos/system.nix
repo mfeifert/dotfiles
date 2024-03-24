6dabab7ec23ad351a202616e531d42ccea3b135c
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-03-24 09:11:04 -0700

    nixos: Fix xkb options

--- nixos/system.nix
+++ nixos/system.nix
@@ -33,9 +33,9 @@
   };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
