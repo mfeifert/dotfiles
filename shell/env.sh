ff5de475a4b1d81a1d2beb285ecd816f4ff5f547
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-04-18 10:01:04 -0700

    Change filepaths to reflect moved docs directory

--- shell/env.sh
+++ shell/env.sh
@@ -16,28 +16,24 @@ esac
 # Environment variables
 
 export EDITOR=hx
-export DOTFILES=$HOME/dotfiles
-export DOCS=$HOME/docs
-export PATH=$HOME/scripts:$PATH
-export PATH=$DOCS/projects/db_scripts:$PATH
-export DB=$DOCS/db
-export LEDGER_FILE=$DOCS/finance/2024.ledger
+export PATH=$HOME/scripts:$HOME/Sync/projects/db_scripts:$PATH
+export DB=~/Sync/db
+export LEDGER_FILE=~/Sync/finance/2024.ledger
 export LESSHISTFILE=-
-export FZF_DEFAULT_COMMAND="rg --files --hidden"
+export FZF_DEFAULT_COMMAND="rg --hidden"
 export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}' --border=rounded --layout=reverse"
 
 # Aliases
 
 alias c='clear'
 alias ll='eza -algh --icons=always --group-directories-first'
-alias py='python3 -q'
+alias p='python3 -q'
 alias watch='watch --color'
 alias p1='ping 1.1.1.1'
-alias d='clear && cd $DOCS'
 alias db='cd $DB'
-alias dot='cd $DOTFILES && $EDITOR'
-alias www='cd $DOCS/www && $EDITOR'
-alias ipq='$DOCS/projects/ip_quiz/ip_quiz.py'
+alias dot='cd ~/dotfiles && $EDITOR'
+alias www='cd ~/Sync/www && $EDITOR'
+alias ipq='~/Sync/projects/ip_quiz/ip_quiz.py'
 
 ## Database scripts
 
@@ -59,10 +55,10 @@ alias is='clear && hledger is --drop 1 -S -b thismonth'
 alias ism='clear && hledger is --drop 1 -MST'
 alias iss='is -2'
 alias issm='ism -2'
-alias hla='hledger -f $DOCS/finance/2020.ledger -f $DOCS/finance/2021.ledger -f $DOCS/finance/2022.ledger -f $DOCS/finance/2023.ledger -f $DOCS/finance/2024.ledger'
+alias hla='hledger -f ~/Sync/finance/2020.ledger -f ~/Sync/finance/2021.ledger -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger -f ~/Sync/finance/2024.ledger'
 alias isa='hla is --drop 1'
 alias issa='isa -2'
 
 export AUTOSQL='select "    ", Miles, "       Miles" from fuel order by Date desc limit 1;'
-alias auto='hla bal auto -f $DOCS/finance/auto.ledger auto --drop 2
-	          sqlite3 -column $DOCS/db/auto.db $AUTOSQL'
+alias auto='hla bal auto -f ~/Sync/finance/auto.ledger auto --drop 2
+	          sqlite3 -column ~/Sync/db/auto.db $AUTOSQL'
