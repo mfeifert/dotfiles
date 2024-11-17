169bb2379dc09c01717e530932e0dc30fa8884e9
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-11-17 12:27:24 -0800

    shell: Add comments to hledger aliases

--- shell/env.sh
+++ shell/env.sh
@@ -47,18 +47,19 @@ alias fuel='fuel.py $DB/auto.db'
 ## ledger & hledger
 
 alias ledger='ledger -f $LEDGER_FILE'
-alias hledger='hledger --pretty -s'
-alias ha='hledger add'
-alias reg='hledger reg -p thismonth'
-alias bs='clear && hledger bs --drop 1'
-alias bsm='bs -M'
-alias is='clear && hledger is --drop 1 -S -b thismonth'
-alias ism='clear && hledger is --drop 1 -MST'
-alias iss='is -2'
-alias issm='ism -2'
-alias hla='hledger -f ~/Sync/finance/2020.ledger -f ~/Sync/finance/2021.ledger -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger -f ~/Sync/finance/2024.ledger'
-alias isa='hla is --drop 1'
-alias issa='isa -2'
+alias hledger='hledger --pretty --strict'
+alias hla='hledger -f ~/Sync/finance/2020.ledger -f ~/Sync/finance/2021.ledger \
+	           -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger \
+		   -f ~/Sync/finance/2024.ledger'  # hledger all
+alias reg='hledger reg -p thismonth'                     # register
+alias bs='clear && hledger bs --drop 1'                  # balance sheet
+alias bsm='bs -M'                                        # balance sheet monthly
+alias is='clear && hledger is --drop 1 -S -b thismonth'  # income statement
+alias ism='clear && hledger is --drop 1 -MST'            # income statement monthly
+alias iss='is -2'                                        # income statement short
+alias issm='ism -2'                                      # income statement short monthly
+alias isa='hla is --drop 1'                              # income statement all
+alias issa='isa -2'                                      # income statement short all
 
 export AUTOSQL="select '    ', Miles, '  Miles' from fuel order by Date desc limit 1;"
 alias auto='hla bal auto -f ~/Sync/finance/auto.ledger auto --drop 2
