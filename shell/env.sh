1c8ff43451d0b1d5b6a868f9f771f3ab87e09746
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-11-24 17:18:31 -0800

    shell: Fix indentation

--- shell/env.sh
+++ shell/env.sh
@@ -1,22 +1,22 @@
 # System-specific configuration
 
 case $(uname -s) in
-    "Darwin")
-        alias ls='ls -Ah --color=always'
-        ;;
-    "Linux")
-        alias ls='ls -Ah --color=auto --group-directories-first'
-        alias ip='ip -c'
-        ;;
-    *)
-        echo "Unknown system type"
-        ;;
+  "Darwin")
+    alias ls='ls -Ah --color=always'
+    ;;
+  "Linux")
+    alias ls='ls -Ah --color=auto --group-directories-first'
+    alias ip='ip -c'
+    ;;
+  *)
+    echo "Unknown system type"
+    ;;
 esac
 
 # Environment variables
 
+export PATH=$HOME/scripts:$PATH
 export EDITOR=nvim
-export PATH=$HOME/scripts:$HOME/projects/db_scripts:$PATH
 export DB=~/Sync/db
 export LEDGER_FILE=~/Sync/finance/2024.ledger
 export LESSHISTFILE=-
@@ -48,14 +48,14 @@ alias n='nutrition.py $DB/test.db'
 alias ledger='ledger -f $LEDGER_FILE'
 alias hledger='hledger --pretty --strict'
 alias hla='hledger -f ~/Sync/finance/2020.ledger -f ~/Sync/finance/2021.ledger \
-	           -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger \
-		   -f ~/Sync/finance/2024.ledger'  # hledger all
-alias reg='hledger reg -p thismonth'                     # register
-alias bs='clear && hledger bs --drop 1'                  # balance sheet
-alias bsm='bs -M'                                        # balance sheet monthly
-alias is='clear && hledger is --drop 1 -S -b thismonth'  # income statement
-alias ism='clear && hledger is --drop 1 -MST'            # income statement monthly
-alias iss='is -2'                                        # income statement short
-alias issm='ism -2'                                      # income statement short monthly
-alias isa='hla is --drop 1'                              # income statement all
-alias issa='isa -2'                                      # income statement short all
+                   -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger \
+                   -f ~/Sync/finance/2024.ledger'       # hledger all
+alias reg='hledger reg -p thismonth'                    # register
+alias bs='clear && hledger bs --drop 1'                 # balance sheet
+alias bsm='bs -M'                                       # balance sheet monthly
+alias is='clear && hledger is --drop 1 -S -b thismonth' # income statement
+alias ism='clear && hledger is --drop 1 -MST'           # income statement monthly
+alias iss='is -2'                                       # income statement short
+alias issm='ism -2'                                     # income statement short monthly
+alias isa='hla is --drop 1'                             # income statement all
+alias issa='isa -2'                                     # income statement short all
