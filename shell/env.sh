e377afad1399f7c48f3c825275f4739b21665c83
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-01-01 12:53:35 -0800

    Change from hard-coded year to "current_year" format
    Change is made for ledger and journal files, as well as Emacs
    functions and shell environment variables relating to these
    files.  This will make it easier to switch over to new years
    in the future.

--- shell/env.sh
+++ shell/env.sh
@@ -20,7 +20,7 @@ export PATH=$HOME/scripts:$PATH
 export PATH=$PATH:$HOME/go/bin
 export EDITOR=nvim
 export DB=~/Sync/db
-export LEDGER_FILE=~/Sync/finance/2024.ledger
+export LEDGER_FILE=~/Sync/finance/current_year.ledger
 export LESSHISTFILE=-
 export HISTSIZE=
 export FZF_DEFAULT_COMMAND='rg --hidden --files'
@@ -62,14 +62,14 @@ alias ledger='ledger -f $LEDGER_FILE'
 alias hledger='hledger --pretty --strict'
 alias hla='hledger -f ~/Sync/finance/2020.ledger -f ~/Sync/finance/2021.ledger \
                    -f ~/Sync/finance/2022.ledger -f ~/Sync/finance/2023.ledger \
-                   -f ~/Sync/finance/2024.ledger'       # hledger all
-alias reg='hledger reg -p thismonth'                    # register
-alias hlp='hledger print -p thismonth'                  # print
-alias bs='clear && hledger bs --drop 1'                 # balance sheet
-alias bsm='bs -M'                                       # balance sheet monthly
-alias is='clear && hledger is --drop 1 -S -b thismonth' # income statement
-alias ism='clear && hledger is --drop 1 -MST'           # income statement monthly
-alias iss='is -2'                                       # income statement short
-alias issm='ism -2'                                     # income statement short monthly
-alias isa='hla is --drop 1'                             # income statement all
-alias issa='isa -2'                                     # income statement short all
+                   -f ~/Sync/finance/2024.ledger -f $LEDGER_FILE' # hledger all
+alias reg='hledger reg -p thismonth'                              # register
+alias hlp='hledger print -p thismonth'                            # print
+alias bs='clear && hledger bs --drop 1'                           # balance sheet
+alias bsm='bs -M'                                                 # balance sheet monthly
+alias is='clear && hledger is --drop 1 -S -b thismonth'           # income statement
+alias ism='clear && hledger is --drop 1 -MST'                     # income statement monthly
+alias iss='is -2'                                                 # income statement short
+alias issm='ism -2'                                               # income statement short monthly
+alias isa='hla is --drop 1'                                       # income statement all
+alias issa='isa -2'                                               # income statement short all
