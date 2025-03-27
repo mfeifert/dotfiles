d4db3e164be6418ba09ac2a8883f13cb6a524444
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-03-26 17:50:53 -0700

    nvim: Add keybinds: C-hjkl for move window focus in all modes

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,20 +20,34 @@ vim.cmd(":syntax off")
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>w', ':set wrap!<cr>')
 vim.keymap.set('n', '<leader>h', ':let @/ = ""<cr>')
-vim.keymap.set('n', '<leader>b', ':lua toggle_background()<cr>')
+vim.keymap.set('n', '<leader>t', ':lua toggle_background()<cr>')
 vim.keymap.set('n', 'H', ':bn<cr>')
 vim.keymap.set('n', 'L', ':bp<cr>')
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<leader>t', ':tabnew<cr>')
 vim.keymap.set('n', '<Tab>', ':tabnext<cr>')
 vim.keymap.set('n', '<S-Tab>', ':tabprevious<cr>')
 vim.keymap.set('i', '<C-f>', '<Right>')
 vim.keymap.set('i', '<C-b>', '<Left>')
 vim.keymap.set('n', '<leader>f', ':FZF<cr>')
-vim.keymap.set('n', '<leader>g', ':Rg<cr>')  -- Requires fzf.vim plugin
+vim.keymap.set('n', '<leader>g', ':Rg<cr>')  -- requires fzf.vim plugin
+vim.keymap.set('n', '<leader>b', ':Buffers<cr>')  -- requires fzf.vim plugin
+
+vim.keymap.set('t', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('t', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('t', '<C-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('i', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('i', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('i', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('i', '<C-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.api.nvim_create_autocmd({ "TermOpen", "WinEnter" }, {
+  pattern = "term://*",
+  command = "startinsert"
+})
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = {"*.c", "*.h"},
