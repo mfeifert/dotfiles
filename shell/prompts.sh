97e0d0d199aca00d8cc207a35d463cbc8abdd661
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-05-03 11:05:58 -0700

    shell: Fix color excape codes in bash prompts

--- shell/prompts.sh
+++ shell/prompts.sh
@@ -3,34 +3,27 @@
 ########
 
 # simple bash prompt for Fedora
-BLUE="\e[1;34m" # bold blue
-BOLD="\e[1;0m"
-RESET="\e[0m"
+BLUE="\[\e[1;34m\]" # bold blue
+BOLD="\[\e[1;0m\]"
+RESET="\[\e[0m\]"
 PS1="${BLUE}[${BOLD}\u@\h \W${BLUE}]${RESET}\\$ "
 
 # simple bash prompt for Arch
-BLUE="\e[1;38;2;51;170;221m"
-RESET="\e[0m"
+BLUE="\[\e[1;38;2;51;170;221m\]"
+RESET="\[\e[0m\]"
 PS1="${BLUE}[${RESET}\u@\h \W${BLUE}]${RESET}\\$ "
 
 # colorful bash prompt from LARBS
-red="\e[1;31m"
-yellow="\e[1;33m"
-green="\e[1;32m"
-blue="\e[1;34m"
-purple="\e[1;35m"
-bold="\e[1;0m"
-reset="\e[0m"
+red="\[\e[1;31m\]"
+yellow="\[\e[1;33m\]"
+green="\[\e[1;32m\]"
+blue="\[\e[1;34m\]"
+purple="\[\e[1;35m\]"
+bold="\[\e[1;0m\]"
+reset="\[\e[0m\]"
 PS1="${red}[${yellow}\u${green}@${blue}\h${purple} \W${red}]${bold}$ ${reset}"
 
-# colorful bash prompt from LARBS with git status indicator
-red="\e[1;31m"
-yellow="\e[1;33m"
-green="\e[1;32m"
-blue="\e[1;34m"
-purple="\e[1;35m"
-bold="\e[1;0m"
-reset="\e[0m"
+# git status indicator with LARBS prompt
 git_status_indicator()
 {
 	if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
