f0b9785280e3a55ffecd671a46236a09309f6afa
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-06-09 21:43:23 -0700

    shell: Move lots of things over from local bashrc

--- shell/env.sh
+++ shell/env.sh
@@ -1,22 +1,12 @@
-# System-specific configuration
-
-case $(uname -s) in
-	"Darwin")
-		alias ls="ls -Ah --color=always"
-		;;
-	"Linux")
-		alias ls="ls -Ahv --color=auto --group-directories-first"
-		alias ip="ip -c"
-		;;
-	*)
-		echo "Unknown system type"
-		;;
-esac
+#
+# bashrc
+#
 
 # Environment variables
 
 export BAT_THEME="ansi"
 export EDITOR="nvim"
+export HISTFILE="$XDG_STATE_HOME/bash/history"
 export HISTSIZE=
 export VERSION_CONTROL="never" # used by GNU indent
 export FZF_DEFAULT_COMMAND="rg --hidden --files"
@@ -34,17 +24,21 @@ alias cp="cp -i"
 alias diff="diff --color=auto"
 alias emacs="emacs -nw"
 alias eza="eza -Agh --icons=auto --group-directories-first --sort Name"
+alias gcc="gcc -Wall --pedantic"
 alias grep="grep --color=auto --exclude-dir=.git"
+alias ip="ip -c"
+alias ls="ls -Ahv --color=auto --group-directories-first"
 alias mv="mv -i"
 alias watch="watch --color"
 
 ## shortcuts
 alias c="clear"
-alias ff="fastfetch"
 alias glk="ghostty +list-keybinds"
 alias glt="ghostty +list-themes"
 alias gr="go run ."
 alias lg="lazygit"
+alias ll="ls -l"
+alias n="(cd $NOTES_DIR && f)"
 alias p="python3 -q"
 alias sc="shellcheck"
 alias t="tmux attach"
@@ -52,14 +46,14 @@ alias tk="tmux kill-session"
 alias tl="tmux ls"
 
 ## utilities
-alias fze="env | sort | fzf --no-preview"
 alias fza="alias | fzf --no-preview"
+alias fze="env | sort | fzf --no-preview"
 alias fzfonts="fc-list : family | sort | column --table --separator=',' | fzf --no-preview"
 alias pp="ping 1.1.1.1"
 
 # Functions
 
-function task()
+task()
 {
 	if [[ $1 == "e" ]]; then
 		(cd "$TASK_DATA_DIR" && nvim -c FZF)
@@ -70,25 +64,25 @@ function task()
 	fi
 }
 
-function tt()
+tt()
 {
 	task-todo "$@"
 	task
 }
 
-function td()
+td()
 {
 	task-daily "$@"
 	[[ $1 != "a" ]] && task
 }
 
-function tm()
+tm()
 {
 	task-monthly "$@"
 	[[ $1 != "a" ]] && task
 }
 
-function f()
+f()
 {
 	file=$(fzf)
 	[[ -n $file ]] && nvim $file
