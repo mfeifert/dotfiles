0525bc95bb1d185b7ccbfaca9bb62a4e117d6ae0
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-12-15 15:59:08 -0800

    nvim: Add autocommand to clear the echo area

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -49,3 +49,9 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = '*.go',
   command = 'silent !go fmt %'
 })
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'InsertEnter' }, {
+  callback = function()
+    vim.cmd('echo')
+  end,
+})
